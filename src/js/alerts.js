ebc57e76b767e67affca69fa62d8864a1442caaa
Rihem BEN SAAD <rihambensaad@gmail.com>
2022-10-17 09:58:29 +0100

    alerts with close option Javascript

--- src/js/alerts.js
+++ src/js/alerts.js
@@ -1,7 +1,7 @@
 const closeElements = document.querySelectorAll('.alert-close');
-closeElements.forEach(function(elements){
-    elements.addEventListenner('click',function(){
-        console.log(elements)
-    })
-    
+closeElements.forEach(function(element){
+    element.addEventListener('click',function(){
+        
+        element.parentElement.classList.add('scale-0')
+    })  
 })
