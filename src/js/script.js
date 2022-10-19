95d84bbf84bffac74990e00a25bfe5dedd09e9d3
Rihem BEN SAAD <rihambensaad@gmail.com>
2022-10-19 23:23:32 +0100

    lightbox with js

---
+++ src/js/script.js
@@ -0,0 +1,42 @@
+const nav = document.getElementById('nav');
+const open = document.getElementByID('open');
+const close = document.getElementByID('close');
+
+//open the navbar
+
+open.addEventListener('click', function(){
+    nav.classList.add('right-0');
+})
+
+//close the navbar
+
+close.addEventlistener('click', function(){
+    nav.classList.remove('right-0')
+})
+
+//lightbox
+
+const lightbox = document.querySelector('.lightbox');
+const lightboxBody = document.querySelector('lightox-body');
+const images = document.querySelectorAll('#gallery');
+images.forEach(function(image){
+    image.addEventlistener('click',function(){
+        lightbox.classList.add('scale-100')
+        const img = document.createElement('img')
+        img.src  = image.src;
+        img.classList.add('w-full');
+        img.classList.add('f-full');
+        img.classList.add('object-cover');
+        if(lightboxBody.children[0]){
+            lightboxBody.removeChild(lightboxBody.children[0])
+        }
+        lightboxBody.appendchild(img)
+
+    })
+})
+
+
+//close lightbox 
+lightbox.addEventListener('click', function(){
+    lightbox.classList.remove('scale-100')
+})
